aee8c7817c4c5f6de08ac5a4da6aabb2f0683432
bandong-jpc <bandong.jpc@gmail.com>
2021-10-19 12:41:10 +0800

    Add for loop to calculate sum of digits

--- main.c
+++ main.c
@@ -2,12 +2,17 @@
 #include <stdlib.h>
 
 int main(){
-  int input = 0;
+  int input = 0, sum = 0;
 
   printf("\nInput number: ");
   scanf("%d", &input);
 
+  for(int i = input; i > 0; i = i / 10){
+    sum = sum + i%10;
+  }
 
+  printf("\n\n%d\n", sum);
+  
 
   return 0;
 
